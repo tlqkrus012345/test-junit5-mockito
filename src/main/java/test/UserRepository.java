86545238cd1e313a5976f144e8530e15b0d06dec
tlqkrus012345 <tlqkrus012345@naver.com>
2023-01-23 21:25:08 +0900

    Repository 테스트 코드 추가

--- src/main/java/test/UserRepository.java
+++ src/main/java/test/UserRepository.java
@@ -4,6 +4,6 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-//    Optional<User> findByEmail(String email);
-//    boolean existByEmail(String email);
+        Optional<User> findByEmail(String email);
+        boolean existsByEmail(String email);
 }
--- src/test/java/test/UserRepositoryTest.java
+++ src/test/java/test/UserRepositoryTest.java
@@ -1,8 +1,12 @@
 package test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
+import java.util.Optional;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -21,6 +25,33 @@ class UserRepositoryTest {
 
         User saveUser = userRepository.save(user);
 
-        Assertions.assertThat(saveUser.getEmail()).isEqualTo(user.getEmail());
+        System.out.println(user + "  " + saveUser);
+        assertThat(saveUser.getEmail()).isEqualTo(user.getEmail());
+        assertThat(saveUser).isEqualTo(user);
+        assertThat(saveUser).isSameAs(user);
+    }
+
+    @Test
+    void find() {
+        userRepository.save(new User("as"));
+
+        List<User> userList = userRepository.findAll();
+
+        User findUser = userList.get(0);
+        assertThat(userList.size()).isEqualTo(1);
+    }
+
+    @Test
+    void findEmail() {
+        User member = userRepository.save(new User("as"));
+        User findMember = userRepository.findByEmail("as").get();
+
+        assertThat(member).isEqualTo(findMember);
+    }
+
+    @Test
+    void existEmail() {
+        User member = userRepository.save(new User("as"));
+        assertTrue(userRepository.existsByEmail("as"));
     }
 }
